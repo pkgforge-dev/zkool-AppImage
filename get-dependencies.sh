77c9846f0084e4a9995c5999a3730fb7e3544f29
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-22 07:30:44 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,6 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
+DEB_LINK=$(
+	wget https://api.github.com/repos/hhanh00/zkool2/releases -O - \
+	  | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*$ARCH.deb"
+)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
@@ -15,4 +19,22 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
-# If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+ar xvf /tmp/app.deb
+tar -xvf ./data.tar.zst
+rm -f ./*.zst ./*.deb
+mv -v ./usr ./AppDir
+
+cp -v ./AppDir/share/applications/zkool.desktop            ./AppDir
+cp -v ./AppDir/share/icons/hicolor/256x256/apps/zkool.png  ./AppDir
+cp -v ./AppDir/share/icons/hicolor/256x256/apps/zkool.png  ./AppDir/.DirIcon
+mv -v ./AppDir/share/zkool                                 ./AppDir/bin
+
+mkdir -p ./AppDir/shared
+mv -v ./AppDir/bin/lib  ./AppDir/shared
+ln -s ../shared/lib     ./AppDir/bin/lib
